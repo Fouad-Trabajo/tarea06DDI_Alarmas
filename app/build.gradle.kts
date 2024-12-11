c391a4998433c896b366a0a48854e4b40b6eee5a
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-12-11 16:12:48 +0100

    chore: update the version of android Studio from Koala to LadyBug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tarea06alarmas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tarea06alarmas"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
