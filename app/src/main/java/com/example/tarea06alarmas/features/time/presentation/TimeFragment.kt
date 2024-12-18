7e8225314f81fcdea9e35db82af18047232296fa
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-12-18 02:57:37 +0100

    feat: The functions of the switch in the items works. Only fonts, and styles

--- app/src/main/java/com/example/tarea06alarmas/features/time/presentation/TimeFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tarea06alarmas.features.time.presentation
-
-import androidx.fragment.app.Fragment
-
-class TimeFragment : Fragment() {
-
-
-}
--- app/src/main/java/com/example/tarea06alarmas/features/time/presentation/adapter/TimeViewHolder.kt
+++ app/src/main/java/com/example/tarea06alarmas/features/time/presentation/adapter/TimeViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.tarea06alarmas.features.time.presentation.adapter
 
+import android.content.Context
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tarea06alarmas.R
 import com.example.tarea06alarmas.databinding.AlarmItemBinding
 import com.example.tarea06alarmas.features.time.domain.model.Time
 
@@ -13,14 +16,22 @@ class TimeViewHolder(private var view: View) : RecyclerView.ViewHolder(view) {
         binding = AlarmItemBinding.bind(view)
         binding.apply {
             titleTime.text = time.title
-            descriptionTime.text = time.description1
+            description1Time.text = time.description1
             description2Time.text = time.description2
-            cardview.setOnClickListener {
-                if (time.switchOn) {
-                    //Toast.makeText(this,"Alarma ${time.title} activa",Toast.LENGTH_SHORT).show
+
+            switchItem.setOnCheckedChangeListener {_,switchOn ->
+                val color = if (switchOn) {
+                    Toast.makeText(view.context,"Alarma ${time.title} activa",Toast.LENGTH_SHORT).show()
+                     view.context.getColor(R.color.md_theme_scrim)
+
                 } else {
-                    //Toast.makeText(this,"Alarma ${time.title} desactivada",Toast.LENGTH_SHORT).show
+                    Toast.makeText(view.context,"Alarma ${time.title} desactivada",Toast.LENGTH_SHORT).show()
+                    view.context.getColor(R.color.md_theme_surfaceDim_highContrast)
                 }
+                titleTime.setTextColor(color)
+                description1Time.setTextColor(color)
+                description2Time.setTextColor(color)
+
             }
         }
     }
